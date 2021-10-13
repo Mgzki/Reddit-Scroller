204dc193bec884394f1bbee7f5e0cd54e3d5da2c
Max Grodzki <maxgrodzki@gmail.com>
2021-10-13 18:42:36 -0400

    Tailwind install

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="bg-blue-200 shadow-lg">
+      <p class="float-left">Burger placeholder</p>
+      <p>Search</p>
+  </nav>
+  <div>
+      Test
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
