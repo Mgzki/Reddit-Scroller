36e49cab08c71c853ad5a0986d1e1ba21d455b0a
Max Grodzki <maxgrodzki@gmail.com>
2021-10-14 14:06:17 -0400

    Created post component

---
+++ src/components/post.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="my-2 col-span-10 sm:col-span-2 bg-gray-100 rounded-xl shadow-lg" >
+      <!-- Post Title -->
+      <div class="flex py-1 bg-gray-200 h-16 rounded-t-lg text-center px-2">
+        <h1 class="font-semibold truncate my-auto">This is a sample title, see what happens when it's even LONGER</h1>
+      </div>
+
+      <!-- Image -->
+      <div class="flex justify-center overflow-hidden flex-row" style="height:24rem">
+        <img src="../assets/test.png" class="flex-1 h-full">
+      </div>
+
+      <!-- Author -->
+      <p class="text-sm align-center bg-gray-200 h-8 rounded-b-lg pt-1">by: Author in:subreddit</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
