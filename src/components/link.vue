70ebb2b2369d96c0dc842fb9d331f18b7485265a
Max Grodzki <maxgrodzki@gmail.com>
2021-10-18 18:03:10 -0400

    Extracted link component

---
+++ src/components/link.vue
@@ -0,0 +1,13 @@
+<template>
+  <a :href="link" class="text-blue-600 underline">
+    <slot/>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+      link: String,
+  }
+}
+</script>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,8 +7,6 @@
         <path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"></path>
       </svg>
       </div>
-      
-
       <!-- Search -->
       <input type="text" class="px-2 py-2 sm:py-4 col-start-3 sm:col-start-2 col-span-8 sm:col-span-10 bg-opacity-0 rounded-md shadow-lg" placeholder="r/">
     </nav>
