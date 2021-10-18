6a32255eec0270df0d6cfa512a5c1ad388d368dc
Max Grodzki <maxgrodzki@gmail.com>
2021-10-18 18:02:37 -0400

    Testing reddit API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,24 +3,9 @@
     <slot>
       <div class="flex max-w-screen-2xl justify-center mx-auto px-2 ">
         <div class="grid grid-cols-6 sm:grid-cols-10 md:grid-cols-9 lg:grid-cols-12 xl:grid-cols-10 grid-flow-row gap-2 pt-6 overflow-y-scroll">
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
+          <template v-for="post in posts" :key="post.data.id">
+            <Post :post="post.data"/>
+          </template>
         </div>
       </div>
     </slot>
@@ -35,6 +20,26 @@ export default {
     Layout,
     Post
   },
+  data () {
+    return {
+      posts: [],
+    }
+  },
+  methods: {
+    async fetchData () {
+      try {
+        const response = await fetch('https://www.reddit.com/r/wallpapers/.json');
+        this.posts = await response.json().then(function(json) {
+          return json.data.children
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  created() {
+    this.fetchData();
+  }
 }
 </script>
 
